2b321c13ef4ff643d35775279790fd5b0dcc1b75
phuoc hacker <anonymousbigtits102@gmail.com>
2022-12-19 10:20:21 +0700

    init test

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -23,5 +23,13 @@
                     <a class="btn btn-primary btn-lg" href="Jumbo action link" role="button">Jumbo action name</a>
                 </p>
             </div>
+            
+            <div class="card">
+                <img class="card-img-top" src="holder.js/100x180/" alt="">
+                <div class="card-body">
+                    <h4 class="card-title">Title</h4>
+                    <p class="card-text">Text</p>
+                </div>
+            </div>
 
 @endsection
